4ddef9ae72228f8d667d219a9a5e3c2932ab663f
Luis Barbosa <luis.barbosa@farfetch.com>
2016-09-18 17:03:56 +0100

    Project creation

---
+++ PluggableMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PluggableMVC.Startup))]
+namespace PluggableMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
