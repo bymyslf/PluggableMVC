2befb4a82b81fa83dc8379109012a3beaf04224b
Luis Barbosa <luis.barbosa@farfetch.com>
2016-09-18 17:21:42 +0100

    Added infrastructure

---
+++ PluginManager/IPlugin.cs
@@ -0,0 +1,20 @@
+﻿namespace PluginManager
+{
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Title of the plugin, can be used as a property to display on the user interface
+        /// </summary>
+        string Title { get; }
+
+        /// <summary>
+        /// Name of the plugin, should be an unique name
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Entry controller name
+        /// </summary>
+        string EntryControllerName { get; }
+    }
+}
