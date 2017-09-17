ef8c780e09f9898882356841ad7fd3b408bc7654
Luis Barbosa <luis.barbosa@farfetch.com>
2017-09-17 15:26:39 +0100

    Added PluginRazorViewEngine

--- PluggableMVC/Global.asax.cs
+++ PluggableMVC/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using PluginFramework;
+using PluginFramework.ViewEngine;
 
 namespace PluggableMVC
 {
@@ -14,10 +15,14 @@ namespace PluggableMVC
         protected void Application_Start()
         {
             PluginBootstrapper.Initialize();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new PluginViewEngine());
         }
     }
 }
---
+++ PluginFramework/ViewEngine/PluginViewEngine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PluginFramework.ViewEngine
+{
+    public class PluginViewEngine : RazorViewEngine
+    {
+        public PluginViewEngine()
+        {
+            this.ExpandViewLocations();
+        }
+
+        private void ExpandViewLocations()
+        {
+            var existingViewPaths = new List<string>(ViewLocationFormats);
+            var existingPartialViewPaths = new List<string>(PartialViewLocationFormats);
+
+            foreach (var plugin in PluginRegistry.Current.Plugins)
+            {
+                var viewsPath = $"~/Plugins/{plugin.Name}/Views/{{1}}/{{0}}.cshtml";
+                var sharedViewsPath = $"~/Plugins/{plugin.Name}/Views/Shared/{{0}}.cshtml";
+
+                existingViewPaths.Add(viewsPath);
+                existingViewPaths.Add(sharedViewsPath);
+
+                existingPartialViewPaths.Add(viewsPath);
+                existingPartialViewPaths.Add(sharedViewsPath);
+            }
+
+            this.ViewLocationFormats = existingViewPaths.ToArray();
+            this.PartialViewLocationFormats = existingPartialViewPaths.ToArray();
+        }
+    }
+}
